1d19fdf9c6b17765f636e91895e38be1ef8561c8
thitnguoixoi <vanlong03062003@gmail.com>
2023-05-17 22:39:41 +0700

    Add project files.

---
+++ ECDHE/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+class Server
+{
+    private TcpListener listener;
+    private ECDiffieHellmanCng ecDh;
+    private byte[] publicKey;
+
+    public Server()
+    {
+        ecDh = new ECDiffieHellmanCng();
+        ecDh.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+        ecDh.HashAlgorithm = CngAlgorithm.Sha256;
+        publicKey = ecDh.PublicKey.ToByteArray();
+    }
+
+    public void Start()
+    {
+        listener = new TcpListener(IPAddress.Any, 1234);
+        listener.Start();
+
+        Console.WriteLine("Server started. Waiting for connections...");
+
+        while (true)
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine($"Client {((IPEndPoint)client.Client.RemoteEndPoint).Address} connected.");
+
+            using (NetworkStream stream = client.GetStream())
+            {
+                stream.Write(publicKey, 0, publicKey.Length);
+
+                byte[] clientPublicKey = new byte[ecDh.PublicKey.ToByteArray().Length];
+                stream.Read(clientPublicKey, 0, clientPublicKey.Length);
+
+                byte[] sharedSecret = ecDh.DeriveKeyMaterial(CngKey.Import(clientPublicKey, CngKeyBlobFormat.EccPublicBlob));
+
+                Console.WriteLine($"Shared key with client {((IPEndPoint)client.Client.RemoteEndPoint).Address}: {BitConverter.ToString(sharedSecret).Replace("-", "")}");
+            }
+
+            client.Close();
+            Console.WriteLine($"Connection with client {((IPEndPoint)client.Client.RemoteEndPoint).Address} closed.");
+        }
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+
+            Server server = new Server();
+            server.Start();
+        }
+        catch (Exception e) { }
+    }
+}
---
+++ client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+
+class Client
+{
+    private byte[] publicKey;
+
+    public Client()
+    {
+    }
+
+    public void Connect(string ipAddress)
+    {
+        TcpClient client = new TcpClient(ipAddress, 1234);
+
+        using (NetworkStream stream = client.GetStream())
+        {
+            byte[] publicKey = new byte[1024];
+            int bytesRead = stream.Read(publicKey, 0, publicKey.Length);
+
+            ECDiffieHellmanCng ecDh = new ECDiffieHellmanCng();
+            ecDh.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            ecDh.HashAlgorithm = CngAlgorithm.Sha256;
+            byte[] clientPublicKey = ecDh.PublicKey.ToByteArray();
+            stream.Write(clientPublicKey, 0, clientPublicKey.Length);
+
+            byte[] sharedSecret = ecDh.DeriveKeyMaterial(CngKey.Import(publicKey, CngKeyBlobFormat.EccPublicBlob));
+
+            Console.WriteLine($"Shared key with server: {BitConverter.ToString(sharedSecret).Replace("-", "")}");
+        }
+
+        client.Close();
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Client client = new Client();
+        client.Connect("127.0.0.1");
+    }
+}
